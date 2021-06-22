f8e09294a7d58a74171aef1875bd953000c5b995
niuzs-alan <zhisheng.niu@jina.ai>
2021-06-22 14:08:17 +0800

    fix: get latest 3 vers both in code_tag and docker_tag

--- batch.sh
+++ batch.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
 
-JINA_VERS=$(git ls-remote --tags https://github.com/jina-ai/jina.git "refs/tags/v*^{}" | cut -d'/' -f3 | cut -d'^' -f1 | cut -d'v' -f2 | sort -Vr | head "-$1")
+JINA_CODE_TAGS=($(git ls-remote --tags https://github.com/jina-ai/jina.git "refs/tags/v*^{}" | cut -d'/' -f3 | cut -d'^' -f1 | cut -d'v' -f2 | sort -Vr ))
+JINA_DOCKER_TAGS=($(wget -q https://registry.hub.docker.com/v1/repositories/jinaai/jina/tags -O -  | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}'))
+
+COMPARE_VERS=$(
+  echo ${JINA_CODE_TAGS[@]} ${JINA_DOCKER_TAGS[@]} | 
+  sed 's/ /\n/g' | 
+  sort | 
+  uniq -d | 
+  sed 's/ /\n/g' | 
+  sort -r | 
+  head -$1
+  )
 
 DONE_VERS=$(cat output/stats.json | jq ".[] |.version" -r)
 
-
-for VER in $JINA_VERS
+for VER in $COMPARE_VERS
 do
 	if [[ $DONE_VERS =~ (^|[[:space:]])$VER($|[[:space:]]) ]]; then
       echo "$VER has been benchmarked, skip!"
