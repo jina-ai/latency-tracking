9d149223016d1a7d51e03f27887c64198f2fc30e
Maksudur Rahman Maateen <maateen@jina.ai>
2021-07-14 11:43:22 +0600

    fix: fixed stats output path
    Signed-off-by: Maksudur Rahman Maateen <maateen@jina.ai>

--- batch.sh
+++ batch.sh
@@ -2,7 +2,7 @@
 
 JINA_VERS=$(git ls-remote --tags https://github.com/jina-ai/jina.git "refs/tags/v*^{}" | cut -d'/' -f3 | cut -d'^' -f1 | cut -d'v' -f2 | sort -Vr | head "-$1")
 
-DONE_VERS=$(cat latency/output/stats.json | jq ".[] |.version" -r)
+DONE_VERS=$(cat /var/output/stats.json | jq ".[] |.version" -r)
 
 
 for VER in $JINA_VERS
@@ -11,6 +11,6 @@ do
       echo "$VER has been benchmarked, skip!"
     else
       docker build --build-arg JINA_VER=$VER -f latency/Dockerfile -t latency-tracking .
-      docker run -v $(pwd)/latency/output:/workspace/output latency-tracking
+      docker run -v /var/output:/app/output latency-tracking
   fi
 done
