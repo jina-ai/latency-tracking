b40f5c7b97e0a4f0ce3dcec2c2de6aeb2251d47b
Maksudur Rahman Maateen <maateen@jina.ai>
2021-07-19 16:28:05 +0600

    feat: versionify folder
    Signed-off-by: Maksudur Rahman Maateen <maateen@jina.ai>

--- benchmark.sh
+++ benchmark.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
-mkdir -p output
+declare -r jina_version=$(jina --version)
+
+mkdir -p ${jina_version}
 
 for file in benchmarks/*.py; do
-    output_json=$(echo $file | sed -r 's/.py/.json/g' | sed -r 's/benchmarks/output/g')
-    output_plot=$(echo $file | sed -r 's/.py/.png/g' | sed -r 's/benchmarks/output/g')
+    output_json=$(echo $file | sed -r 's/.py/.json/g' | sed -r "s/benchmarks/${jina_version}/g")
+    output_plot=$(echo $file | sed -r 's/.py/.png/g' | sed -r "s/benchmarks/${jina_version}/g")
     cmdbench --save-json=${output_json} --save-plot=${output_plot} python3 ${file}
 done
