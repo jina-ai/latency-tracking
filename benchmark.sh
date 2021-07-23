c59dea28d309889e021021a6b9c1035ecbb0a582
Maksudur Rahman Maateen <maateen@jina.ai>
2021-07-23 18:06:12 +0600

    fix: benchmark script
    Signed-off-by: Maksudur Rahman Maateen <maateen@jina.ai>

--- benchmark.sh
+++ benchmark.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 
-declare -r jina_version=$(jina --version)
-
-mkdir -p ${jina_version}
+mkdir -p outputs
 
 for file in benchmarks/*.py; do
-    output_json=$(echo $file | sed -r 's/.py/.json/g' | sed -r "s/benchmarks/${jina_version}/g")
-    output_plot=$(echo $file | sed -r 's/.py/.png/g' | sed -r "s/benchmarks/${jina_version}/g")
+    output_json=$(echo $file | sed -r 's/.py/.json/g' | sed -r "s/benchmarks/outputs/g")
+    output_plot=$(echo $file | sed -r 's/.py/.png/g' | sed -r "s/benchmarks/outputs/g")
     cmdbench --save-json=${output_json} --save-plot=${output_plot} python3 ${file}
 done
